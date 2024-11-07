19d784306b4ea97a838e02a03dda2fc1b5323355
Akls958 <74070771+Akls958@users.noreply.github.com>
2024-11-07 20:56:47 +0000

    Mundo guardado 2024-11-07

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-# Versiones disponibles
+1# Versiones disponibles
 # forge, mohist, fabric, vanilla, paper
 
 # Puedes instalar mohist después de instalar forge desde el menú de gestionar
@@ -9,7 +9,7 @@
 # Código          Lugar
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
-# au		      Australia (Sydney)
+# au		      Australia (Sydney)s
 # eu		      Europa (Frankfurt)
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
